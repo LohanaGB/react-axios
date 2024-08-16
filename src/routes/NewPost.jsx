671d3b7c41169e0d6f739e47a22e01fa96f46518
Lohana Pereira <lohana.gvsp@gmail.com>
2024-08-16 16:29:22 -0300

    att

--- src/routes/NewPost.jsx
+++ src/routes/NewPost.jsx
@@ -1,11 +1,33 @@
-import React from 'react'
+import React from 'react';
+import "./NewPost.css";
 
 const NewPost = () => {
   return (
-    <div>
-        <h1>
-            NewPost
-        </h1>
+    <div className="newPost">
+      <h2>Inserir Novo Post:</h2>
+
+      <form>
+        <div className="formControl">
+          <label htmlFor="title">Título:</label>
+          <input
+            type="text" 
+            name="title" 
+            id="title" 
+            placeholder="Digite o título" 
+          />
+        </div>
+        
+        <div className="formControl">
+          <label htmlFor="body">Conteúdo:</label>
+          <textarea 
+            name="body"
+            id="body" 
+            placeholder="Digite o conteúdo"
+          ></textarea>
+        </div>
+        <input type="submit" value="Criar Post" className="btn" />
+      </form>
+
     </div>
   )
 }
