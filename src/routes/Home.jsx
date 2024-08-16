b1a6ad1e6516c3a929d7a4be3bf9323a586e4f83
Lohana Pereira <lohana.gvsp@gmail.com>
2024-08-16 16:04:44 -0300

    att

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useState, useEffect } from 'react';
-import axios from 'axios';
+// import axios from 'axios';
+import blogFetch from '../axios/config';
 import { Link } from 'react-router-dom';
 import "./Home.css";
 
@@ -11,11 +12,11 @@ const Home = () => {
   const getPosts = async() => {
     try {
 
-      const response = await axios.get('https://jsonplaceholder.typicode.com/posts');
+      const response = await blogFetch.get('/posts');
       const data = response.data;
-      console.log(data);
-      // console.log(response);
+      // console.log(data);
 
+      setPosts(data);
     } catch (error) {
       console.log(error);
     }
@@ -26,10 +27,17 @@ const Home = () => {
   }, [])
 
   return (
-    <div>
-      <h1>
-        Home
-      </h1>
+    <div className="home">
+      <h1>Últimos posts</h1>
+      {posts.length === 0 ? (<p>Carregando...</p>) : (
+        posts.map((post)=> (
+          <div className="post" key={post.id}>
+            <h2>{post.title}</h2>
+            <p>{post.body}</p>
+            <Link to={`/posts/${post.id}`} className="btn">Ler mais</Link>
+          </div>
+        ))
+      )}
     </div>
   )
 }
